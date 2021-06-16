ae410627394dbee552f22e2ae2f35a5c998db0c7
tsurumoto <tsurumotokentarou@gmail.com>
2021-06-16 09:57:53 +0900

    Initial Commit

---
+++ DivideMusicPlayer/DivideMusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DivideMusicPlayerApp.swift
+//  DivideMusicPlayer
+//
+//  Created by 鶴本賢太朗 on 2021/06/16.
+//
+
+import SwiftUI
+
+@main
+struct DivideMusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
